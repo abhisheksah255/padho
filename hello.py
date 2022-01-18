42da5b2b3148f3894772c5d78590ff6c93e926ff
ABHISHEK SAH <abhisheksah255@gmail.com>
2022-01-18 18:23:33 +0530

    added some  more code to my python

--- hello.py
+++ hello.py
@@ -1,1 +1,6 @@
-print"hello my name is Abhishek kumar sah"
+print("Hello my name is Abhishek kumar sah")
+a=(input("what's your name:"))
+if a=="abhishek":
+    print("Abhishek ")
+else:
+    print("your name is :",a)
